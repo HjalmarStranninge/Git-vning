6da7495d67c1f59a3f45b9689d2776a67e508d73
Huan <huanyang.ooi@gmail.com>
2024-01-04 11:26:58 +0100

    added bird class

---
+++ GitÖvning/Bird.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitÖvning
+{
+    internal class Bird
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public Bird(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+}
