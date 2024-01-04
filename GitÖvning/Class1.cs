8f548ab35c74acac02bbc17cd787554a2897c07c
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-01-04 11:25:46 +0100

    Added the fantastic 'Cat'-class.

---
+++ GitÖvning/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitÖvning
+{
+    internal class Cat
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public Cat(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name} says meow!");
+        }
+    }
+}
