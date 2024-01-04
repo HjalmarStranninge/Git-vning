beca4eae339e38b2251bebc2fdff6ee3e1d7a4a8
lukas99o <lukas99o@hotmail.com>
2024-01-04 11:32:35 +0100

    Added Garen From League of Legends

---
+++ GitÖvning/Knight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitÖvning
+{
+    internal class Knight
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Strength { get; set; }
+
+        public void BattleSound()
+        {
+            Console.WriteLine("For Demacia!!!");
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nAge: {Age}\nHP: {HP}\nStrength: {Strength}");
+        }
+    }
+}
